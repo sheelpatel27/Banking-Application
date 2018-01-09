f1f5fabae1d67314338c00caa20276510d478b5f
sheelpatel27 <sheelpatel27@gmail.com>
2018-01-09 09:50:15 -0500

    Update account.php

--- account.php
+++ account.php
@@ -2,10 +2,10 @@
 
 
 
-$hostname = "sql2.njit.edu"; 			
-$username = "dtb25" ;
-$project  = "dtb25" ;
-$password = "Deesha1994" ;
+$hostname = ""; 			
+$username = "" ;
+$project  = "" ;
+$password = "" ;
 
 ?>
 
